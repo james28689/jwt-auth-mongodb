3eec02054e53cec42beec03df5f2faeb7633d32b
James Watling <james.watling@gmail.com>
2021-10-01 12:18:40 +0100

    Removed body-parser

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 require("dotenv").config();
@@ -11,8 +10,8 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 const Role = db.role;
